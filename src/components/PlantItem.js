1909ff13e8895ce4202e2f2ba9cc6d98bab74fc3
Reididsorg <bruno.grosdidier@gmail.com>
2024-07-19 11:48:40 +0200

    Ajout de la fonctionnalité de mise au panier des produits avec calcul du total et affichage détaillé. #develop

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -2,13 +2,14 @@ import CareScale from './CareScale'
 import '../styles/PlantItem.css'
 
 function handleClick(plantName) {
-    console.log(plantName);
+    alert(`Vous voulez acheter 1 ${plantName}? Très bon choix 🌱✨`)
 }
 
-function PlantItem({ id, cover, name, water, light, isSpecialOffer }) {
+function PlantItem({ id, cover, name, water, light, price, isSpecialOffer }) {
 
     return (
         <li key={id} className='lmj-plant-item' onClick={() => handleClick(name)}>
+            <span className='lmj-plant-item-price'>{price}€</span>
             <img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`}/>
             {name}
             {isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,6 +1,6 @@
 import { plantList } from '../datas/plantList'
-import '../styles/ShoppingList.css';
 import PlantItem from './PlantItem';
+import '../styles/ShoppingList.css';
 
 function ShoppingList({ cart, updateCart }) {
     // Petite précision : categories nous vient de la partie précédente pour récupérer toutes les catégories uniques de plantes.
@@ -11,6 +11,21 @@ function ShoppingList({ cart, updateCart }) {
         []
     )
 
+    function addToCart(name, price) {
+        const currentPlantSaved = cart.find((plant) => plant.name === name)
+        if (currentPlantSaved) {
+            const cartFilteredCurrentPlant = cart.filter(
+                (plant) => plant.name !== name
+            )
+            updateCart([
+                ...cartFilteredCurrentPlant,
+                { name, price, amount: currentPlantSaved.amount + 1 }
+            ])
+        } else {
+            updateCart([...cart, { name, price, amount: 1 }])
+        }
+    }
+
     return (
         <div className='lmj-shopping-list'>
             <ul>
@@ -19,10 +34,17 @@ function ShoppingList({ cart, updateCart }) {
                 ))}
             </ul>
             <ul className='lmj-plant-list'>
-                {plantList.map(({ id, cover, name, water, light, isSpecialOffer }) => (
+                {plantList.map(({ id, cover, name, water, light, price, isSpecialOffer }) => (
                     <div key={id}>
-                        <PlantItem cover={cover} name={name} water={water} light={light} isSpecialOffer={isSpecialOffer} />
-                        <button onClick={() => updateCart(cart + 1)}>Ajouter</button>
+                        <PlantItem
+                            cover={cover}
+                            name={name}
+                            water={water}
+                            light={light}
+                            price={price}
+                            isSpecialOffer={isSpecialOffer}
+                        />
+                        <button onClick={() => addToCart(name, price)}>Ajouter</button>
                     </div>
                 ))}
             </ul>
